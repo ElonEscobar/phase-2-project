1b3a322d733a99cd446b229d87079f34380ed8a3
Dan Muendo <danwalker254254@gmail.com>
2022-10-05 04:08:07 -0400

    updated file images

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -2,7 +2,7 @@ import React from "react"
 import Back from "../common/Back"
 import RecentCard from "../home/recent/RecentCard"
 import "../home/recent/recent.css"
-import img from "../images/about.jpg"
+import img from "../image/about.jpg"
 
 const Blog = () => {
   return (
